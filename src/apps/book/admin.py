ae678670222f041cbbd5ff1da4de56193729e4f3
zahra <znajafiyan@gmail.com>
2025-08-21 21:32:54 +0330

    Add Django Admin for Book, BookContributor, and Category

--- src/apps/book/admin.py
+++ src/apps/book/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
+from apps.book.models import Book, BookContributor, Category
 
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "slug"]
 
 
+@admin.register(BookContributor)
 class BookContributorAdmin(admin.ModelAdmin):
     list_display = ["id", "full_name"]
 
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "category__name", "book_contributor__full_name", "publishera"]
     raw_id_fields = ['category', "book_contributor"]
