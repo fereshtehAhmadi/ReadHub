09501740cde8d4c9ef2276318d980705db80ce7a
zahra <znajafiyan@gmail.com>
2025-08-21 20:26:50 +0330

    add book app

--- src/apps/book/apps.py
+++ src/apps/book/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class App1Config(AppConfig):
-    name = "apps.app_1"
-    verbose_name = _("App_1")
+class BookConfig(AppConfig):
+    name = "apps.book"
+    verbose_name = _("Book")
